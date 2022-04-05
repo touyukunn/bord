b3f28805b63e8b0dda4a7e5f294a0c001cec2c0d
touyukunn <touyukunn86@gmail.com>
2022-04-05 20:35:35 +0900

    DB接続情報更新

--- matsumoto.php
+++ matsumoto.php
@@ -32,7 +32,7 @@ try{
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
         PDO::MYSQL_ATTR_MULTI_STATEMENTS => false,
      );
-   $pdo = new PDO('mysql:charset=UTF8;dbname='.DB_name.';host='.DB_host,DB_user);
+   $pdo = new PDO('mysql:charset=UTF8;dbname='.DB_name.';host='.DB_host,DB_user,DB_pass);
 
 }catch(PDOException $e){
 
